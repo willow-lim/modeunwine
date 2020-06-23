540ddcac11ee6401f1bafc557a18d5135746e67f
willow-lim <67330521+willow-lim@users.noreply.github.com>
2020-06-24 00:11:06 +0900

    Add files via upload

---
+++ modeunwine/src/main/java/kr/co/modeunwine/ModeunwineApplication.java
@@ -0,0 +1,13 @@
+package kr.co.modeunwine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeunwineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeunwineApplication.class, args);
+	}
+
+}
